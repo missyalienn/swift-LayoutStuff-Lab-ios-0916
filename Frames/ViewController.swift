8f33cc02bcd9c26558c1bbb1aee3eb4e133eda60
Missy Allan <missyallan@Melissas-MacBook-Air.local>
2016-10-12 17:58:36 -0400

    layout stuff weird cat

--- Frames/ViewController.swift
+++ Frames/ViewController.swift
@@ -10,7 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     var yellowSquare: UIView?
+    
+    
 
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var frameXSlider: UISlider!
+    
+    @IBOutlet weak var frameYSlider: UISlider!
+    
+    @IBOutlet weak var boundsXSlider: UISlider!
+    
+    @IBOutlet weak var boundsYSlider: UISlider!
+    
+    @IBOutlet weak var frameXLabel: UILabel!
+
+    @IBOutlet weak var frameYLabel: UILabel!
+    
+    @IBOutlet weak var boundsXLabel: UILabel!
+    
+    @IBOutlet weak var boundsYLabel: UILabel!
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabels()
